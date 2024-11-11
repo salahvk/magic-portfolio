7f8a3c62ef5821de0317846458d24a445be625ad
salahvk <salahkoyilandy@gmail.com>
2024-11-11 17:38:31 +0530

    revert file

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,16 @@
 import mdx from '@next/mdx';
 import createNextIntlPlugin from 'next-intl/plugin';
+
+const withMDX = mdx({
+    extension: /\.mdx?$/,
+    options: { },
+});
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+};
+
 export default withNextIntl(withMDX(nextConfig));
-
-try {
-    const withMDX = mdx({
-        extension: /\.mdx?$/,
-        options: { },
-    });
-
-    const withNextIntl = createNextIntlPlugin();
-
-    /** @type {import('next').NextConfig} */
-    const nextConfig = {
-        pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-    };
-
-    
-
-} catch (error) {
-    if (!(error instanceof Error)) {
-        throw new Error('Unexpected error type: ' + error);
-    } else {
-        throw error;
-    }
-}
